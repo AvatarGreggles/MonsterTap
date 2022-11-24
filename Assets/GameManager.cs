112e9299d682f619ba6fde6823fcd9dc97e81eeb
Greggles <gregory@wetransfer.com>
2022-11-24 19:09:09 +0100

    Add pewter gym

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -40,6 +40,12 @@ public class GameManager : MonoBehaviour
         stageText.text = stage.name.ToString();
         stageProgressText.text = stageProgressCounter + " / " + stage.stageLength;
 
+        if (stage.isCutscene)
+        {
+            gameState = GameState.Cutscene;
+            CutsceneManager.instance.PlayCutscene(stage.cutscene);
+        }
+
     }
 
     // Update is called once per frame
